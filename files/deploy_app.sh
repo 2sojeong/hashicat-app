23448545c36bd50eb8a35aa7aadffd39c1404c89
2sojeong <dlgur200123@naver.com>
2024-05-24 05:04:40 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,6 +12,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQprNJX_z9OEB9N9w1Ycgh7XHTuRTmeAMYbPQ&s" alt="Image not found"></img></center>
+  <a href="https://ibb.co/XSBGShk"><img src="https://i.ibb.co/Hnv9nWp/kovengers.png" alt="kovengers" border="0"></a>
+  
   <center><h2>KOVENGERS</h2></center>
   Welcome to KOVENGERS app.
   <!-- END -->
